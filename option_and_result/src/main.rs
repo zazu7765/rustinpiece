94b3012e0af041c035a2b44aa3ea306578306cea
zazu7765 <ohmyzazu@gmail.com>
2023-06-30 02:11:17 -0400

    custom option example
    did this a while back but forgot to commit it so here it is now...

--- option_and_result/src/main.rs
+++ option_and_result/src/main.rs
@@ -3,17 +3,30 @@ use std::fmt::Debug;
 fn main() {
     let has_no_fifth = vec![1, 2, 3, 4];
     let has_fifth = vec![1, 2, 3, 4, 5];
-    custom_option_handler(take_fifth_value(has_fifth));
-    custom_option_handler(take_fifth_value(has_no_fifth));
+    // option one: custom option handler
+    custom_option_handler(take_fifth_value(&has_fifth));
+    custom_option_handler(take_fifth_value(&has_no_fifth));
+    // option two: if statement
+    let vec_of_fifths = vec![has_no_fifth, has_fifth];
+    for vec in vec_of_fifths {
+        let num = take_fifth_value(&vec);
+        if num.is_some() {
+            println!("Found {:?}", num.unwrap());
+        } else {
+            println!("Found nothing...");
+        }
+    }
 }
 
-fn take_fifth_value(value: Vec<u32>) -> Option<u32> {
+// Option function
+fn take_fifth_value(value: &Vec<u32>) -> Option<u32> {
     if value.len() < 5 {
         return None;
     }
     Some(value[4])
 }
 
+// Option handler
 fn custom_option_handler<T: Debug + Copy>(option: Option<T>) {
     match option {
         Some(t) => println!("Found {:?}", t),
